bc05d1b9c59474c4773bad41f153cdcd72e1cf9d
Aryandesai1 <aryandesai34@gmail.com>
2025-06-07 13:48:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,16 @@ TO_EMAIL = os.getenv("TO_EMAIL")
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Allow requests from the frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
